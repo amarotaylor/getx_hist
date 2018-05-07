bc708210e291aee4b4a6827c2565c0985eb36177
amaro@broadinstitute.org <amaro@broadinstitute.org>
2018-05-07 13:35:11 -0400

    initial commit

---
+++ tf_model_functions.py
@@ -0,0 +1,291 @@
+import tensorflow as tf
+from keras import backend as K
+import numpy as np
+import re
+import sys
+import glob
+import math
+import yaml
+
+
+# example yaml file :
+## YAML structure
+"""
+layer0:
+    type: 'conv'
+    width: 12
+    output: 64
+    pool: 4
+layer1:
+    type: 'incept'
+    downsample: 16
+    output: 16
+    width1: 4
+    width2: 12
+    pool: 4
+layer2:
+    type: 'fc'
+    output: 1280
+layer3:
+    type: 'dropout'
+layer4:
+    type: 'fc'
+    output: 2
+"""
+def erprint(in_text):
+    sys.stderr.write(in_text+'\n')
+
+
+def read_and_decode(filename_queue):
+    reader = tf.TFRecordReader()
+
+    _, serialized_example = reader.read(filename_queue)
+
+    image = decode(serialized_example)
+
+    return {'image': image}
+
+
+def decode(serialized_example, height=100, width=100, dtype = tf.float32):
+    features = tf.parse_single_example(
+        serialized_example, features={
+            'image_raw': tf.FixedLenFeature([], tf.string),
+            'file_name': tf.FixedLenFeature([], tf.string)
+        })
+
+    image = tf.decode_raw(features['image_raw'], tf.uint8)
+
+    image_shape = tf.stack([height, width, 3])
+
+    image = tf.reshape(image, image_shape)
+
+    processed_example = tf.image.resize_image_with_crop_or_pad(image=image,
+                                                               target_height=height,
+                                                               target_width=width)
+    return tf.cast(processed_example, dtype= dtype)
+
+
+
+def inputs(file_regex, batch_size, num_epochs, num_threads=2, shuffle=True, dtype=tf.float32):
+    if not num_epochs:
+        num_epochs = None
+    filenames = glob.glob(file_regex)
+    with tf.name_scope('input'):
+        min_after_dequeue = 200
+        capacity = min_after_dequeue + ( num_threads + 3 ) * batch_size
+        if shuffle:
+            filename_queue = tf.train.string_input_producer(filenames,num_epochs=num_epochs,shuffle=True)
+            example_list = [ read_and_decode(filename_queue, dtype=dtype)
+                             for _ in range(num_threads) ]
+            images = tf.train.shuffle_batch_join(
+                example_list,
+                batch_size=batch_size,
+                capacity=capacity,
+                min_after_dequeue=min_after_dequeue)
+        else:
+            filename_queue = tf.train.string_input_producer(filenames,num_epochs=num_epochs,shuffle=False)
+            example_list = read_and_decode(filename_queue, dtype=dtype)
+            images = tf.train.batch(
+                example_list,
+                batch_size=batch_size,
+                num_threads=num_threads,
+                capacity=capacity,
+                allow_smaller_final_batch=True)
+        flat_targets = tf.reshape(images,[-1,100*100*3])
+        return images, flat_targets
+
+
+def restore_checkpoint(sess,saver,save_dir):
+    erprint("Model checkpoints found at {}".format(save_dir))
+    tf.train.get_checkpoint_state(save_dir)
+    erprint("Recovered checkpoint state.")
+    most_recent_save = tf.train.latest_checkpoint(save_dir)
+    erprint("Loading checkpoint: {}".format(most_recent_save))
+    saver.restore(sess, most_recent_save)
+    erprint("Model restored")
+
+
+
+def weight_variable(shape, name, stddev=0.1, dtype=tf.float32):
+    with tf.device('/cpu:0'):
+        final = tf.get_variable(name=name,shape=shape,dtype=dtype,
+            initializer=tf.truncated_normal_initializer(mean=0,stddev=stddev))
+    return final
+
+def bias_variable(shape, name, dtype=tf.float32):
+    with tf.device('/cpu:0'):
+        final = tf.get_variable(name=name,shape=shape,dtype=dtype,
+            initializer=tf.constant_initializer(0.1))
+    return final
+
+
+def variable_summaries(var):
+    tensor_name = re.sub('{}_[0-9]*/'.format('tower'), '', var.op.name)
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar(tensor_name + '/mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar(tensor_name + '/stddev',stddev)
+        tf.summary.scalar(tensor_name + '/max', tf.reduce_max(var))
+        tf.summary.scalar(tensor_name + '/min', tf.reduce_min(var))
+        tf.summary.scalar(tensor_name + '/sparsity', tf.nn.zero_fraction(var))
+        tf.summary.histogram(tensor_name + '/activations', var)
+
+
+def average_gradients(tower_grads):
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        grads = []
+        for g, _ in grad_and_vars:
+            expanded_g = tf.expand_dims(g,0)
+            grads.append(expanded_g)
+        grad = tf.concat(values=grads, axis=0)
+        grad = tf.reduce_mean(grad, 0)
+        var = grad_and_vars[0][1]
+        grad_and_var = (grad, var)
+        average_grads.append(grad_and_var)
+    return average_grads
+
+def training(loss, learning_rate):
+    with tf.name_scope('train'):
+        optimizer = tf.train.AdamOptimizer(learning_rate)
+        global_step = tf.Variable(0, name='global_step', trainable=False)
+        train_op = optimizer.minimize(loss, global_step=global_step)
+    return train_op, global_step
+
+# difference of decoded image from target image
+# y_hat and targets_flat should be batch_size x (length*width*channels)
+def img_loss(y_hat, targets_flat,scale_factor = 100*100*3):
+    with tf.name_scope('image_loss'):
+        return tf.divide(tf.reduce_sum(tf.squared_difference(y_hat, targets_flat), 1), scale_factor)
+
+# kl divergence from standard normal
+def kl_loss(sd,mn):
+    with tf.name_scope('kl_loss'):
+        return -0.5 * K.sum(1 + sd - K.square(mn) - K.exp(sd), axis=-1)
+
+class layer_maker:
+    def __init__(self,in_tensor,in_chn,in_width,dtype=tf.float32,training=True,dformat='channels_last', hidden = 1000):
+        self.Ops = {}
+        self.in_tensor = in_tensor
+        self.in_chn = in_chn
+        self.in_width = in_width
+        self.training = training
+        self.dtype = dtype
+        self.dformat = dformat
+        self.hidden = hidden
+
+    def conv2d(self,x, f, k, name, stride=1, padding='same', format='channels_last',activation = tf.nn.leaky_relu):
+        ''' wrapper for tf.layers.conv2d'''
+        layer = tf.layers.conv2d(x, filters=f, kernel_size=k, strides=stride, padding=padding,
+                                 name=name, data_format=format,
+                                 activation=activation)
+        return layer
+
+    def deconv2d(self,x, f, k, name, stride=1, padding='same', format='channels_last',activation = tf.nn.leaky_relu):
+        ''' wrapper for tf.layers.conv2d_transpose'''
+        layer = tf.layers.conv2d_transpose(x, filters=f, kernel_size=k, padding='same', strides=stride,
+                                           data_format=format
+                                           , name=name,activation=activation)
+        return layer
+
+    def max_pool_2d(self,x, p, name, stride=1, format='channels_last', padding='valid'):
+        '''wrapper for tf.layers.max_pooling2d'''
+        layer = tf.layers.max_pooling2d(x, pool_size=p,
+                                        strides=stride,
+                                        padding=padding, data_format=format)
+        return layer
+
+    def fully_connected(self,x, u, name, activation = tf.nn.leaky_relu):
+        ''' wrapper for tf.layers.dense'''
+        layer = tf.layers.dense(x, units = u,activation = activation,
+                                 name=name)
+        return layer
+
+    def sampling(self,z_mean, z_log_var):
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self.hidden), mean=0.,
+                                  stddev=1.)
+        return z_mean + K.exp(z_log_var / 2) * epsilon
+
+    def make_layer(self,l_info,l_index,last=False):
+        ''' uses YAML file to generate layers '''
+        layer_name = "layer_{}_{}".format(l_info['type'],l_index)
+        layer_id = "layer_{}".format(l_index)
+        data_format = self.dformat
+
+        if l_info['type'] == 'variational':
+            self.mn = tf.layers.dense(self.in_tensor, units=self.hidden, activation=tf.nn.leaky_relu, name='encode_mean')
+            self.sd = 0.5 * tf.layers.dense(self.in_tensor, units=self.hidden, activation=tf.nn.leaky_relu, name='encode_sd')
+            layer = self.sampling(self.mn, self.sd)
+            out_chn = self.hidden
+            out_width = 1
+            variable_summaries(self.mn)
+            variable_summaries(self.sd)
+
+        assert l_info['type'] in ['conv','incept','deconv','fc','dropout','flatten','maxpool']
+
+        if l_info['type'] == 'conv':
+            try:
+                l_info['stride']
+            except KeyError:
+                l_info['stride'] = 1
+            with tf.name_scope(layer_id):
+                layer = self.conv2d(x = self.in_tensor, f= l_info['filters'], k = l_info['kernel_size'],
+                                name = layer_name, stride=l_info['stride'])
+            out_chn = l_info['filters']
+            out_width = self.in_width
+
+        elif l_info['type'] == 'deconv':
+            try:
+                l_info['stride']
+            except KeyError:
+                l_info['stride'] = 1
+            with tf.name_scope(layer_id):
+                layer = self.deconv2d(self.in_tensor,f = l_info['filters'],
+                                    k = l_info['kernel_size'],name=layer_name, stride=l_info['stride'],padding='same')
+            out_chn = l_info['filters']
+            out_width = int(math.ceil( self.in_width / float(l_info['stride'])))
+        elif l_info['type'] == 'fc':
+
+            layer = self.fully_connected(self.in_tensor, l_info['units'], name = layer_name,
+                                         activation = tf.nn.leaky_relu)
+            out_chn = l_info['units']
+            out_width = 1
+        elif l_info['type'] == 'flatten':
+
+            with tf.name_scope(layer_id):
+
+                out_chn = self.in_width * self.in_chn
+                out_width = 1
+
+                layer = tf.reshape(self.in_tensor, shape=[-1, out_chn], name=layer_name)
+
+        elif l_info['type'] == 'maxpool':
+            try:
+                l_info['stride']
+            except KeyError:
+                l_info['stride'] = 1
+
+            with tf.name_scope(layer_id):
+                layer = self.max_pool_2d(self.in_tensor,p = l_info['pool'], name=layer_name,
+                                    stride=l_info['stride'], format='channels_last', padding='same')
+                out_width = int(math.ceil( (self.in_width - float(l_info['pool']) / float(l_info['stride'])))) + 1
+                out_chn = self.in_chn
+        self.in_tensor = layer
+        self.in_chn = out_chn
+        self.in_width = out_width
+        self.Ops[l_index] = layer
+
+
+def inference(images, nn_architecture, dtype=tf.float32, training=True):
+    #print images.shape
+    in_tensor = images
+    in_chn = 3
+    in_width = 100
+    #print in_tensor.shape
+    builder = layer_maker(in_tensor, in_chn, in_width, dtype=dtype, training=training)
+    for layer_index in range(len(nn_architecture.keys())):
+        l_info = nn_architecture['layer{}'.format(layer_index)]
+        builder.make_layer(l_info,layer_index)
+    return builder.sd,builder.mn,builder.in_tensor
---
+++ training.py
@@ -0,0 +1,156 @@
+import tensorflow as tf
+import sys
+import os
+import time
+import glob
+import argparse
+import yaml
+
+from tf_model_functions import *
+
+parser = argparse.ArgumentParser(description='Train CNN for classification of variant selection.')
+parser.add_argument('training_data', type=str, help='Directory containing training data in tfrecords format.')
+parser.add_argument('checkpoint_dir', type=str, help='Directory where model checkpoints will be saved.')
+parser.add_argument('--epochs', '-e', default=20, type=int, help='Training epochs.')
+parser.add_argument('--dropout_frequency', '-d', default=0.50, type=float,
+                    help='Set frequency for dropout normalization during training.')
+parser.add_argument('--batch_size', '-b', default=2000, type=int, help='Batch size for training.')
+parser.add_argument('--input_threads', '-p', default=2, type=int,
+                    help='Number of input threads to use (must be 2 or more).')
+parser.add_argument('--num_gpus', '-g', default=2, type=int, help='specify number of GPUs to use.')
+parser.add_argument('--summary_dir', '-s', default='/tmp/tensorflow_summaries', help='Data containing summary info')
+parser.add_argument('--checkpoint_frequency', '-c', default=5000, type=int,
+                    help="Number of steps between checkpoint saves.")
+parser.add_argument('--collect_run_metadata', '-m', action='store_true',
+                    help="Flag to store run metadata during training. Warning: buggy API component.")
+parser.add_argument('--nn_architecture', '-nn_a', type=str, help="YAML file containing nn_architecture specs.")
+parser.add_argument('--learning_rate',default=0.01, type=float)
+parser.add_argument('--epsilon',default = 1e-8, type = float)
+args = parser.parse_args()
+
+
+def run_training(args):
+    # Define nn_arch
+    nn_path = args.nn_architecture
+    nn_arch = yaml.safe_load(open(nn_path, 'r'))
+    print nn_arch
+
+    # set run-parameters
+    num_epochs = args.epochs
+    save_file = os.path.join(args.checkpoint_dir, 'my_model')
+    set_dtype = tf.float32
+    adam_lr = args.learning_rate
+    adam_ep = args.epsilon
+    # Make checkpoint dir if doesn't exist
+    try:
+        os.makedirs(args.checkpoint_dir)
+        RESTORE = False
+    except OSError:
+        if not os.path.isdir(args.checkpoint_dir):
+            raise
+        else:
+            if glob.glob(save_file + "*"):
+                RESTORE = True
+            else:
+                RESTORE = False
+    # Make summary dir if doesn't exist
+    try:
+        os.makedirs(args.summary_dir)
+    except OSError:
+        if not os.path.isdir(args.summary_dir):
+            raise
+    #
+    with tf.Graph().as_default(), tf.device('/cpu:0'):
+        input_pipes = inputs(os.path.join(args.training_data, '*tfr'),
+                             batch_size=args.batch_size,
+                             num_epochs=num_epochs,
+                             num_threads=args.input_threads,
+                             shuffle=True,
+                             dtype=set_dtype)
+        images,flat_target = input_pipes
+        batch_queue = tf.contrib.slim.prefetch_queue.prefetch_queue(
+            images, capacity=2 * args.batch_size * args.num_gpus)
+        global_step = tf.get_variable(name='global_step', shape=[],
+                                      initializer=tf.constant_initializer(0),
+                                      trainable=False)
+        optimizer = tf.train.AdamOptimizer(learning_rate=adam_lr, epsilon=adam_ep)
+        tower_grads = []
+        with tf.variable_scope(tf.get_variable_scope()):
+            for index in xrange(args.num_gpus):
+                with tf.device('/gpu:{}'.format(index)):
+                    with tf.name_scope('tower_{}'.format(index)) as scope:
+                        image_batch, target_batch = batch_queue.dequeue()
+                        sd, mn, decoded = inference(image_batch, nn_arch,
+                                           dtype=set_dtype, training=True)
+                        loss_op = tf.reduce_mean(img_loss(y_hat=decoded,targets_flat=target_batch) + kl_loss(sd=sd,mn=mn))
+                        tf.get_variable_scope().reuse_variables()
+                        summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
+                        grads = optimizer.compute_gradients(loss_op)
+                        tower_grads.append(grads)
+        grads = average_gradients(tower_grads)
+        for grad, var in grads:
+            if grad is not None:
+                summaries.append(tf.summary.histogram(var.op.name + '/gradients', grad))
+        train_op = optimizer.apply_gradients(grads, global_step=global_step)
+        summary_op = tf.summary.merge_all()
+        init_op = tf.group(tf.global_variables_initializer(),
+                           tf.local_variables_initializer())
+        vars_to_restore = tf.contrib.slim.get_variables_to_restore()
+        saver = tf.train.Saver(vars_to_restore, keep_checkpoint_every_n_hours=2, max_to_keep=8)
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+            summary_writer = tf.summary.FileWriter(args.summary_dir, sess.graph)
+            sess.run(init_op)
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            if RESTORE == True:
+                sys.stderr.write("Restoring Checkpoint\n")
+            try:
+                incr = 0
+                step = 0
+                start_time = time.time()
+                sys.stderr.write("Start time: {}\n".format(start_time))
+                while not coord.should_stop():
+                    incr += 1
+                    if ((step % 500) == 498) and (args.collect_run_metadata):
+                        run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                        run_metadata = tf.RunMetadata()
+                        _, loss_value, summary, step = sess.run([train_op, loss_op, summary_op, global_step],
+                                                                # feed_dict={keep_prob: args.dropout_frequency},
+                                                                options=run_options,
+                                                                run_metadata=run_metadata)
+                        duration = time.time() - start_time
+                        summary_writer.add_run_metadata(run_metadata, 'step%03d' % step)
+                        summary_writer.add_summary(summary, step)
+                        sys.stderr.write("Step {}: loss = {} ({} sec)\n".format((step + 1),
+                                                                                loss_value,
+                                                                                duration))
+                        start_time = time.time()
+                    elif (step % 100) == 98:
+                        _, loss_value, summary, step = sess.run([train_op, loss_op, summary_op, global_step]  # ,
+                                                                # feed_dict={keep_prob: args.dropout_frequency}
+                                                                )
+                        duration = time.time() - start_time
+                        summary_writer.add_summary(summary, step)
+                        sys.stderr.write("Step {}: loss = {} ({} sec)\n".format((step + 1),
+                                                                                loss_value,
+                                                                                duration))
+                        start_time = time.time()
+                    else:
+                        _, loss_value, summary, step = sess.run([train_op, loss_op, summary_op, global_step]  # ,
+                                                                # feed_dict={keep_prob: args.dropout_frequency}
+                                                                )
+                        duration = time.time() - start_time
+                    if (step % args.checkpoint_frequency) == (args.checkpoint_frequency - 1):
+                        saver.save(sess, save_file, global_step=global_step)
+            except tf.errors.OutOfRangeError:
+                sys.stderr.write('Done training for {} epochs, {} steps.\n'.format(num_epochs,
+                                                                                   step + 1))
+            finally:
+                coord.request_stop()
+            coord.join(threads)
+            saver.save(sess, save_file, global_step=global_step)
+            summary_writer.add_summary(summary, step)
+
+
+if __name__ == '__main__':
+    run_training(args)
