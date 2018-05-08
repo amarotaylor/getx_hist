b8f3b7902f6f7da7ee3955f1d506b5acb5372214
amaro@broadinstitute.org <amaro@broadinstitute.org>
2018-05-08 16:47:24 -0400

    fixes

--- tf_model_functions.py
+++ tf_model_functions.py
@@ -181,7 +181,7 @@ class layer_maker:
         self.hidden = hidden
         self.batch_size = batch_size
         self.mn = []
-        self.std = []
+        self.sd = []
 
     def conv2d(self,x, f, k, name, stride=1, padding='same', format='channels_last',activation = tf.nn.leaky_relu):
         ''' wrapper for tf.layers.conv2d'''
