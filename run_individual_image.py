65cbc7517b777a35a097dc7dc887a41a0f5cd2c6
amaro@broadinstitute.org <amaro@broadinstitute.org>
2018-05-10 13:51:56 -0400

    reverting path changes

--- run_individual_image.py
+++ run_individual_image.py
@@ -61,7 +61,7 @@ def evaluate_image(args):
             raise
     #
     with tf.Graph().as_default(), tf.device('/cpu:0'):
-        input_pipes = inputs(args.image,
+        input_pipes = inputs(os.path.join(args.training_data, '*tfrecords'),
                              batch_size=args.batch_size,
                              num_epochs=num_epochs,
                              num_threads=args.input_threads,
